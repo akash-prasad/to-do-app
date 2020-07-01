2e51693419299273f3c2078cc727b26041cc3942
Kamandula, Akash Prasad <kamandap@miamioh.edu>
2020-06-30 20:59:34 -0400

    database migrations and seeds

---
+++ database/migrations/2020_06_30_210538_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class TasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('task_id');
+            $table->string('task_name');
+            $table->unsignedBigInteger('priority_id');
+            $table->string('status');
+            $table->timestampTz('completed_on', 0)->nullable();
+            $table->timestampTz('created_at', 0)->nullable();
+            $table->timestampTz('deleted_at', 0)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
---
+++ database/migrations/2020_06_30_234820_create_priorities_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrioritiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('priorities', function (Blueprint $table) {
+            $table->bigIncrements('priority_id');
+            $table->string('priority_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('priorities');
+    }
+}
